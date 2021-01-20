d41b0bf667890fb5a6787a66c5f83618d7474698
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2021-01-20 19:04:48 +0530

    Add files via upload

---
+++ HTML_IDE.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root=Tk()
+root.minsize(650,650)
+root.maxsize(650,650)
+
+open_img=ImageTk.PhotoImage(Image.open("open.png"))
+save_img=ImageTk.PhotoImage(Image.open("save.png"))
+run_img=ImageTk.PhotoImage(Image.open("download.jfif"))
+
+label_file_name=Label(root,text="File name")
+label_file_name.place(relx=0.28,rely=0.03,anchor=CENTER)
+
+input_file_name=Entry(root)
+input_file_name.place(relx=0.46,rely=0.03,anchor=CENTER)
+
+my_text=Text(root,height=35,width=80,bg="slate gray")
+my_text.place(relx=0.5,rely=0.55,anchor=CENTER)
+
+
+root.mainloop()
